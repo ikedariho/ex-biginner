b8a771566caf3889c18bf5c8bf3d2a760069e9c4
ikedariho <riho.ikeda@rakus-partners.co.jp>
2019-11-05 13:31:57 +0900

    Exam1

---
+++ src/main/java/com/example/Controller/Exam01Controller.java
@@ -0,0 +1,41 @@
+package com.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Form.Exam01Form;
+
+@Controller
+@Repository("/exam01")
+
+public class Exam01Controller {
+
+	@ModelAttribute
+	public Exam01Form setUpForm() {
+
+		return new Exam01Form();
+	}
+	
+	@RequestMapping("")
+	
+	public String index() {
+		return "exam01";
+	}
+	
+	@RequestMapping("/result01")
+	public String rusult01(Exam01Form exam01Form,Model model) {
+		
+		model.addAttribute("name",exam01Form.getName());
+		
+		
+		
+		
+		return "exam01-result";
+		
+		
+	}
+
+}
---
+++ src/main/java/com/example/Form/Exam01Form.java
@@ -0,0 +1,23 @@
+package com.example.Form;
+
+public class Exam01Form {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "exam01Form [name=" + name + ", getName()=" + getName() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+
+}
